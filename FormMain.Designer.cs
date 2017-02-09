69db40ad4f1a69e1a15fbe51345161c5a1f78e77
Rafael <rafakwolf@gmail.com>
2017-02-09 13:27:31 -0200

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aplicativo rodando";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 101);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Network Proxy Notifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WindowsFormsApplication5
+{
+    public partial class FormMain : Form
+    {
+        private const string UserRoot = "HKEY_CURRENT_USER";
+        private const string Subkey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+        private const string KeyName = UserRoot + "\\" + Subkey;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var result = Registry.GetValue(KeyName, "ProxyEnable", 0);
+
+            if (result.ToString() == "1")
+            {
+                timer1.Enabled = false;
+
+                notifyIcon1.Visible = true;
+                notifyIcon1.Icon = SystemIcons.Information;
+                notifyIcon1.Click += NotifyIcon1_Click;
+
+                notifyIcon1.BalloonTipTitle = @"Proxy foi ativado";
+                notifyIcon1.BalloonTipText = @"Atenção seu proxy de rede foi ativado, clique para desativar";
+
+                notifyIcon1.ShowBalloonTip(5000); // 5 segundos
+            }
+        }
+
+        private void NotifyIcon1_Click(object sender, EventArgs e)
+        {
+            Registry.SetValue(KeyName, "ProxyEnable", 0, RegistryValueKind.DWord);
+            timer1.Enabled = true;
+            notifyIcon1.Visible = false;
+        }
+    }
+}
